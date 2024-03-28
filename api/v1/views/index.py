9b8382a5d24d90d3549f27921fa9497edcc0b385
oovaa <omarvx211@gmail.com>
2024-03-28 11:06:37 +0200

    task 4 all good till now at 28-11

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
 
 from api.v1.views import app_views
+from models import storage
 from flask import jsonify
+from models.city import City
+from models.amenity import Amenity
+from models.user import User
+from models.state import State
+from models.place import Place
+from models.review import Review
 
 
-@app_views.route('/status', methods=['GET'])
-def api_status():
-    return jsonify({"status": "OK"})
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    result = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    return jsonify(result)
