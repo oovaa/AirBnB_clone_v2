057924e09d7b97ec65360f49c8c70f1741e32396
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-03-29 22:36:34 +0300

    T7 refactor create_city

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -33,30 +33,33 @@ def get_city(city_id):
 def delete_city(city_id):
     """Deletes a city from the database"""
     city = storage.get(City, city_id)
-    if city is None:
-        abort(404)
-    city.delete()
-    storage.save()
-    return jsonify({}), 200
+    if city:
+        storage.delete(city)
+        storage.save()
+        return jsonify({}), 200
+    else:
+        return abort(404)
 
 
 @app_views.route('/states/<string:state_id>/cities', methods=['POST'],
                  strict_slashes=False)
 def create_city(state_id):
     """Creates a new city in the database"""
+    if request.content_type != 'application/json':
+        return abort(404,'Not a JSON')
     state = storage.get(State, state_id)
-    if state is None:
-        abort(404)
+    if not state:
+        return abort (404)
     if not request.get_json():
-        return jsonify({'error': 'Not a JSON'}), 400
-    if 'name' not in request.get_json():
-        return jsonify({'error': 'Missing name'}), 400
+        return jsonify(400, 'Not a JSON')
+    data = request.get_json()
+    if 'name' not in data:
+        return abort(400, 'Missing name')
+    data['state_id'] = state_id
 
-    js = request.get_json()
-    obj = City(**js)
-    obj.state_id = state.id
-    obj.save()
-    return jsonify(obj.to_dict()), 201
+    city = City(**data)
+    city.save()
+    return jsonify(city.to_dict()), 201
 
 
 @app_views.route('/cities/<string:city_id>', methods=['PUT'])
