41000721c7eb0f3b7b090a79ccef65cc0c398b86
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-03-29 15:53:42 +0300

    Task 9 part 1 (get,get,del)

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+"""Users view module"""
+
 from flask import jsonify, abort, request
 from models.city import City
 from models.state import State
@@ -8,12 +11,14 @@ from models import storage
 
 @app_views.route('/users', methods=['GET'])
 def get_users():
+    """Return all users in the system."""
     users = storage.all(User)
     return jsonify([user.to_dict() for user in users.values()])
 
 
 @app_views.route('/users/<user_id>', methods=['GET'])
 def get_user(user_id):
+    """Return a user in the system."""
     u = storage.get(User, user_id)
     if u is None:
         abort(404)
@@ -22,14 +27,23 @@ def get_user(user_id):
 
 @app_views.route('/users/<user_id>', methods=['DELETE'])
 def delete_user(user_id):
-    pass
+    """Delete a user given its id."""
+    u = storage.get(User, user_id)
+    if u is None:
+        abort(404)
+    u.delete()
+    storage.save()
+    return jsonify({}), 200
 
 
 @app_views.route('/users', methods=['POST'])
 def create_user():
-    pass
+    """Create a new user with data from the request."""
+    if not request.json:
+        return
 
 
 @app_views.route('/users/<user_id>', methods=['PUT'])
 def update_user(user_id):
+    """Update a user given its id and data from the request."""
     pass
