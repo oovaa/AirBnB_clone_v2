82ae53c95e1426dd676ad4c7a285e5923fdd705e
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-03-31 23:59:08 +0300

    Password Hashing: Edit to_dict in basemodel  L73

--- models/base_model.py
+++ models/base_model.py
@@ -70,9 +70,7 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
-        if 'password' in new_dict and (not include_password
-                                       or not isinstance(models.storage,
-                                                         FileStorage)):
+        if getenv("HBNB_TYPE_STORAGE") == "db" and "password" in new_dict:
             del new_dict['password']
         return new_dict
 
